aa735b62706c324399f1ecafc717bcdd8e620942
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2024-05-09 10:06:01 -0400

    hwinfo-based device serial implementation

--- src/main.c
+++ src/main.c
@@ -1,32 +1,41 @@
 //! @file
 
-// clang-format off
-#include "memfault/ports/zephyr/include_compatibility.h"
-
-#include MEMFAULT_ZEPHYR_INCLUDE(kernel.h)
-#include MEMFAULT_ZEPHYR_INCLUDE(logging/log.h)
 #include <string.h>
+#include <zephyr/drivers/hwinfo.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
+#include "app_version.h"
 #include "memfault/components.h"
 #include "memfault/ports/zephyr/http.h"
-// clang-format on
 
 LOG_MODULE_REGISTER(mflt_app, LOG_LEVEL_DBG);
 
-MEMFAULT_STATIC_ASSERT(
-  sizeof(CONFIG_MEMFAULT_PROJECT_KEY) > 1,
-  "No Memfault Project Key Set! Please set CONFIG_MEMFAULT_PROJECT_KEY in prj.conf");
+const char *memfault_zephyr_get_device_id(void) {
+  uint8_t dev_id[16] = {0};
+  static char dev_id_str[sizeof(dev_id) * 2 + 1];
 
-sMfltHttpClientConfig g_mflt_http_client_config = {
-  .api_key = CONFIG_MEMFAULT_PROJECT_KEY,
-};
+  // Check if the device id has already been initialized
+  if (dev_id_str[0]) {
+    return dev_id_str;
+  }
+
+  // Obtain the device id
+  ssize_t length = hwinfo_get_device_id(dev_id, sizeof(dev_id));
+
+  // Render the obtained serial number in hexadecimal representation
+  for (size_t i = 0; i < length; i++) {
+    (void)snprintf(&dev_id_str[i * 2], sizeof(dev_id_str), "%02x", dev_id[i]);
+  }
+
+  return dev_id_str;
+}
 
 void memfault_platform_get_device_info(sMemfaultDeviceInfo *info) {
   *info = (sMemfaultDeviceInfo){
-    .device_serial = "DEMOSERIAL",
-    .software_type = "zephyr-app",
-    .software_version =
-      CONFIG_MEMFAULT_EXAMPLE_SOFTWARE_VERSION "+" ZEPHYR_MEMFAULT_EXAMPLE_GIT_SHA1,
+    .device_serial = memfault_zephyr_get_device_id(),
+    .software_type = "app",
+    .software_version = APP_VERSION_STRING "+" ZEPHYR_MEMFAULT_EXAMPLE_GIT_SHA1,
     .hardware_version = CONFIG_BOARD,
   };
 }
